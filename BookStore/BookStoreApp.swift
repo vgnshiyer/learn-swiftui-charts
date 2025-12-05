0c354ccb658ec697ef77f9fb7c31378348ee064e
Vignesh Iyer <vgnshiyer@gmail.com>
2025-12-05 13:39:39 -0800

    Initial Commit

---
+++ BookStore/BookStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreApp.swift
+//  BookStore
+//
+//  Created by Vignesh Iyer on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookStoreTests/BookStoreTests.swift
@@ -0,0 +1,16 @@
+//
+//  BookStoreTests.swift
+//  BookStoreTests
+//
+//  Created by Vignesh Iyer on 12/5/25.
+//
+
+import Testing
+
+struct BookStoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
