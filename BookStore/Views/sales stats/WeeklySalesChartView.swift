1bb46d24b9ff9ace6f0fb7828c16c90be43c685e
Vignesh Iyer <vgnshiyer@gmail.com>
2025-12-06 12:52:42 -0800

    weekly sales chart tooltip

--- BookStore/Views/sales stats/WeeklySalesChartView.swift
+++ BookStore/Views/sales stats/WeeklySalesChartView.swift
@@ -12,11 +12,60 @@ struct WeeklySalesChartView: View {
     
     var salesViewModel: SalesViewModel
     
+    @State private var rawSelectedDate: Date? = nil
+    @Environment(\.calendar) var calendar
+    
+    var selectedDateValue: (day: Date, sales: Int)? {
+        if let rawSelectedDate {
+            return salesViewModel.salesByWeek.first {
+                let startOfWeek = $0.day
+                let endOfWeek = calendar.date(byAdding: .day, value: 6, to: startOfWeek) ?? Date()
+                
+                return (startOfWeek...endOfWeek).contains(rawSelectedDate)
+            }
+        } else {
+            return nil
+        }
+    }
+    
+    private let monthInterval: TimeInterval = 3600 * 24 * 30
+    
     var body: some View {
-        Chart(salesViewModel.salesByWeek, id: \.day) { saleData in
-            BarMark(x: .value("Week", saleData.day, unit: .weekOfYear),
-                    y: .value("Sales", saleData.sales))
-            .foregroundStyle(Color.blue.gradient)
+        VStack(alignment: .leading) {
+            Chart {
+                ForEach(salesViewModel.salesByWeek, id: \.day) { saleData in
+                    BarMark(x: .value("Week", saleData.day, unit: .weekOfYear),
+                            y: .value("Sales", saleData.sales))
+                    .foregroundStyle(Color.blue.gradient)
+                    .opacity(selectedDateValue?.day == nil || selectedDateValue?.day == saleData.day ? 1 : 0.5)
+                }
+                    
+                if let rawSelectedDate {
+                    RuleMark(x: .value("Selected Date", rawSelectedDate, unit: .weekOfYear))
+                        .foregroundStyle(Color.gray.opacity(0.3))
+                        .zIndex(-1)
+                        .annotation(position: .top, spacing: 0, overflowResolution: .init(x: .fit(to: .chart), y: .disabled)) {
+                            selectionTooltip
+                        }
+                }
+            }
+            .chartXSelection(value: $rawSelectedDate)
+        }
+    }
+    
+    @ViewBuilder
+    var selectionTooltip: some View {
+        if let selectedDateValue {
+            VStack {
+                Text(selectedDateValue.day.formatted(.dateTime.month().day()))
+                Text("\(selectedDateValue.sales) sales")
+            }
+            .padding(6)
+            .background {
+                RoundedRectangle(cornerRadius: 4)
+                    .fill(.white)
+                    .shadow(color: .accentColor, radius: 2)
+            }
         }
     }
 }
